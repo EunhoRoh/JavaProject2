424b714409645d008156da16c239bcc21e24125e
EUNHOROH <deu04189@gmail.com>
2021-07-02 21:15:37 +0900

    Readme파일 수정

--- src/main/java/FileService.java
+++ src/main/java/FileService.java
@@ -39,24 +39,6 @@ public class FileService {
                 i++;
             }
             bufReader.close();
-            // bufferedReader 사용
-
-            // fileUtils 사용
-//            List<String> lines = FileUtils.readLines(new File("data.txt"));
-//            for(int i =0 ; i < lines.size(); i++){
-//                StringTokenizer st = new StringTokenizer(lines.get(i) , "/");
-//
-//                String name = st.nextToken().trim();
-//                int korScore = Integer.parseInt(st.nextToken().trim());
-//                int engScore = Integer.parseInt(st.nextToken().trim());
-//                int mathScore = Integer.parseInt(st.nextToken().trim());
-//                String grade = Person.calculateGrade(korScore, engScore, mathScore);
-//                String regDate = st.nextToken().trim();
-//
-//                list.add(new Person(i, name, korScore, engScore, mathScore, grade, regDate));
-//            }
-            // fileUtils 사용
-
         }catch (FileNotFoundException e) {
             System.out.println("schedule.txt 파일이 존재하지 않습니다.");
         }catch(IOException e){
@@ -77,30 +59,7 @@ public class FileService {
                 FileUtils.write (file,d.getName() + " / " + d.getPart() + " / " + d.getExercise() + " / " + d.getNumOfSets()
                         + " / " + d.getReps()  + " / " + d.getWeight() + " / " + d.getRegiDate()+ "\n", true);
             }
-
-
             // fileUtils 사용
-
-            // FileWriter 사용
-//            File file = new File("data.txt");
-//            FileWriter fw = new FileWriter(file, false);
-//            for (Person p: this.list) {
-//                fw.write(p.getName() + " / " + p.getKorScore() + " / " + p.getEngScore() + " / " + p.getMathScore() + " / " + p.getRegDate() + "\n");
-//            }
-//            fw.flush();
-//            fw.close();
-            // FileWriter 사용
-
-            // BufferedWriter, FileWriter 사용
-//            File file = new File("data.txt");
-//            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-//            for (Person p: this.list) {
-//                bw.write(p.getName() + " / " + p.getKorScore() + " / " + p.getEngScore() + " / " + p.getMathScore() + " / " + p.getRegDate());
-//                bw.newLine();
-//            }
-//            bw.close();
-            // BufferedWriter, FileWriter 사용
-
         }catch (FileNotFoundException e) {
             System.out.println("schedule.txt 파일이 존재하지 않습니다.");
         }catch(IOException e){
